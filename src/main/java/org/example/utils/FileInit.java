109da0f3052d610dbc8e6774f92597380b6da586
Roberto <roberto.fard@gmail.com>
2023-04-03 17:09:50 +0200

    reate the utils classes
    FileInit.java
    RetrieveProject.java

---
+++ src/main/java/org/example/utils/FileInit.java
@@ -0,0 +1,13 @@
+package org.example.utils;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileInit {
+            public FileInit(String file){
+                
+
+        }
+
+}
---
+++ src/main/java/org/example/utils/RetrieveProject.java
@@ -0,0 +1,21 @@
+package org.example.utils;
+
+public class RetrieveProject {
+        private String project, projectDir;
+        public String getProjName() {
+            return project;
+        }
+        public String getProjDirName() {
+            return projectDir;
+        }
+        public RetrieveProject(boolean bool){
+            if(bool){
+                this.project ="AVRO";
+                this.projectDir = "C:\\Users\\Roberto\\Documents\\GitHub\\avro";
+            }else{
+                this.project ="BOOKKEEPER";
+                this.projectDir = "C:\\Users\\Roberto\\Documents\\GitHub\\bookkeeper";
+            }
+        }
+
+}
