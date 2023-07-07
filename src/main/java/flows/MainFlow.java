cb6071e4f7101b44dd2c792260b640af3ce43a52
Roberto <roberto.fard@gmail.com>
2023-07-07 19:02:42 +0200

    finish

--- src/main/java/flows/MainFlow.java
+++ src/main/java/flows/MainFlow.java
@@ -10,14 +10,12 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
 import java.io.File;
 import java.util.List;
-
 import static utils.CsvUtils.CsvReader.leggiCsv;
 import static utils.ManageRelease.getHalfRelease;
 import static utils.ManageRelease.retrieveReleases;
 
 public class MainFlow {
     public static void main(String[] args) throws Exception {
-
         RetrieveJiraTickets retrieveJiraTickets = new RetrieveJiraTickets();
         RetrieveProject project = new RetrieveProject(true); //true = AVRO false = BOOKKEEPER
         String localPath = project.getProjDirName();
@@ -26,7 +24,6 @@ public class MainFlow {
         List<Release> halfReleases = getHalfRelease(releases);
         ProportionFlow proportionFlow = new ProportionFlow(retrieveJiraTickets, project.getProjName());
         List<FileTouched> javaClasses = new MetricsAndBugginessFlow(repo, proportionFlow.getIssueTicketsWithIV(), halfReleases, project.getProjName()).getJavaClassesList2();
-        List<FileTouched> javaClasses = leggiCsv("C:\\Users\\Roberto\\Documents\\GitHub\\deliverable-ISW2\\", project.getProjName()+"-results_M1.csv" );
         new WekaFlow(project, halfReleases, javaClasses);
     }
 }
--- src/main/java/flows/WekaFlow.java
+++ src/main/java/flows/WekaFlow.java
@@ -3,6 +3,7 @@ package flows;
 import control.Weka;
 import entity.FileTouched;
 import entity.Release;
+import utils.CostSensitivityType;
 import utils.RetrieveProject;
 
 
@@ -10,7 +11,7 @@ import java.util.List;
 
 public class WekaFlow {
     public WekaFlow(RetrieveProject project, List<Release> halfReleases, List<FileTouched> javaClasses) throws Exception {
-        Weka weka = new Weka(project, halfReleases, javaClasses, false, null, null);
+        Weka weka = new Weka(project, halfReleases, javaClasses, true, null, CostSensitivityType.SENSITIVITY_LEARNING);
         weka.wekaWork();
     }
 }
